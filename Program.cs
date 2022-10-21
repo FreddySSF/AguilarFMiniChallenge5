b2c65fa81c459100e60cb9411b035d476d84f7f3
FreddySSF <fernandoaguilarssf@gmail.com>
2022-10-21 10:19:23 -0700

    AguilarF MiniChallenge5 Endpoint Update

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // Mini Challenge #5 Madlib Endpoint
 // Creating a mad lib console project. The user will input twelve different inputs when prompted.
 // Then the program will output a short story based off of the inputs given by the user. Include data validation.
-// Peer Reviewed By: Jessie Lamzon
+// Peer Reviewed By: Jessie Lamzon - The code ran well and ran with no errors. The story was funny, everything was spaced well too. Data validation was great and worked well with each entry. Only thing i saw was when playing again i pressed enter and it exited. good job!
 
 Console.Clear();
 
