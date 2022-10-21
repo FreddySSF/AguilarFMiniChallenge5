8e009598f035cf216a81d782e6d6c5f6c3abde67
FreddySSF <fernandoaguilarssf@gmail.com>
2022-10-20 20:08:54 -0700

    AguilarF MiniChallenge5 Endpoint

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 // Mini Challenge #5 Madlib Endpoint
 // Creating a mad lib console project. The user will input twelve different inputs when prompted.
 // Then the program will output a short story based off of the inputs given by the user. Include data validation.
+// Peer Reviewed By: 
 
 Console.Clear();
 
